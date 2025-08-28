c28b8c5bc50c098f4a34f5e8c2f9f75856f0164a
The game fire <thegamefire@outlook.com>
2025-08-28 22:58:21 +0200

    Started Development on Better UI

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import ProfileSidebar from '$lib/components/ProfileSidebar.svelte';
+	import ProfileSummary from '$lib/components/ProfileSummary.svelte';
+</script>
+
+
+<div class="flex flex-col w-4/5 items-center justify-center m-auto">
+	<ProfileSummary />
+	<ProfileSidebar class="w-md max-w-full" />
+</div>
