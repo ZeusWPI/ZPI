b08de8089ee0713e5b4475fbb58d0fb3409ed8f7
The game fire <thegamefire@outlook.com>
2025-08-28 22:58:21 +0200

    Started Development on Better UI

---
+++ ui/src/lib/components/ProfileSidebar.svelte
@@ -0,0 +1,24 @@
+<script>
+	import SocialLink from '$lib/components/sidebar-components/SocialLink.svelte';
+
+	const userDescription = 'This is a placeholder for the about section of a profile. Here, you would typically find a personal introduction and a short biography. The user might describe their professional journey, their core interests and passions, and what they are currently working on. This text serves as a way to present a narrative beyond just a list of skills or job titles.';
+
+	const links = {
+		'GitHub': 'https://github.com/lolcatjpg',
+		'MatterMost': ''
+	};
+
+</script>
+<div class={`${$$props.class || ''}`}>
+	<span class="font-bold">About</span>
+	<br>
+	<p>{userDescription}</p>
+	<br>
+	<div class="flex">
+		{#each Object.entries(links) as [service, link]}
+			<SocialLink {service} {link} />
+		{/each}
+	</div>
+</div>
+
+
---
+++ ui/src/lib/components/ProfileSummary.svelte
@@ -0,0 +1,13 @@
+<script>
+	const profileImg = 'https://zpi.zeus.gent/image/385';
+	const username = 'caturn';
+</script>
+
+<div class="flex flex-row w-full flex-wrap justify-center">
+	<img class="size-96 rounded-4xl" src="{profileImg}" alt="Profile">
+
+	<span
+		class="text-4xl grow min-w-64 text-center md:text-left">{ username.charAt(0).toUpperCase() + username.slice(1) }</span>
+
+
+</div>
