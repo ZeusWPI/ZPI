2bcc98ccd21b8f4420ac1103dbf003303b7cb32f
The game fire <thegamefire@outlook.com>
2025-08-29 17:34:15 +0200

    Finished Basics of Mobile UI

---
+++ ui/src/lib/components/Footer.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Tooltip from '$lib/components/Tooltip.svelte';
+	import svelteIcon from '$lib/assets/svelte.svg';
+	import rustIcon from '$lib/assets/rust.svg';
+
+
+	let backend_version = '2.0.1';
+	let frontend_version = '1.0.1';
+</script>
+
+
+<div class="flex flex-row justify-between md:grid md:grid-cols-3">
+	<!-- Versions -->
+	<div class="flex flex-row items-center gap-2">
+		<Tooltip text="Backend Version">
+			<div class="flex flex-row items-center">
+				<img class="size-4" src={rustIcon} alt="Rust" />
+				v{backend_version}
+			</div>
+		</Tooltip>
+		<Tooltip text="Frontend Version">
+			<div class="flex flex-row items-center">
+				<img class="size-4" src={svelteIcon} alt="Svelte" />
+				v{frontend_version}
+			</div>
+		</Tooltip>
+	</div>
+
+	<!-- Center / Right on Mobile -->
+	<span class="text-right md:text-center">Made with ❤️ by ZeusWPI</span>
+
+</div>
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,11 +2,22 @@
 	import ProfileSidebar from '$lib/components/ProfileSidebar.svelte';
 	import ProfileSummary from '$lib/components/ProfileSummary.svelte';
 	import ShowcaseDisplay from '$lib/components/ShowcaseDisplay.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
+<Navbar />
 
 <div class="flex flex-col w-4/5 items-center justify-center m-auto">
-	<ProfileSummary />
+	<div class="flex flex-row w-full flex-wrap justify-center mb-8">
+		<ProfileSummary />
+	</div>
 	<ProfileSidebar class="w-md max-w-full" />
 	<ShowcaseDisplay />
+</div>
+
+<div class="flex flex-row justify-center mt-5 mb-3">
+	<div class="w-5/6">
+		<Footer />
+	</div>
 </div>
