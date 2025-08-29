8cd76bb87e333e32acccc2d4d7d4c3cf09c964d6
The game fire <thegamefire@outlook.com>
2025-08-29 17:34:15 +0200

    Finished Basics of Mobile UI

---
+++ ui/src/lib/components/Navbar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import logoutIcon from '$lib/assets/logout.svg';
+
+	type User = { id: number, username: string, mattermost: string, name: string }
+	let user: User = {
+		id: 385,
+		username: 'caturn',
+		name: 'Saturn Casaer',
+		mattermost: 'caturn'
+	};
+</script>
+
+
+<div class=" w-full text-white left-0 top-0 p-2 flex flex-row justify-between items-center">
+	<!-- Left Content -->
+	<div class="flex flex-row items-center">
+		<img class="size-8" src={favicon} alt="ZPI Icon">
+	</div>
+	<!-- Right Content -->
+	<div class="flex flex-row items-center">
+		<span class="text-lg">{user.username}</span>
+		<a href="/logout">
+			<img class="size-6 mx-2 invert" src={logoutIcon} alt="Logout" />
+		</a>
+	</div>
+</div>
+
+<style>
+    div {
+        background-color: #ff7f00;
+    }
+</style>
--- ui/src/lib/components/ProfileSummary.svelte
+++ ui/src/lib/components/ProfileSummary.svelte
@@ -1,22 +1,38 @@
-<script>
+<script lang="ts">
 	import ProfileTag from '$lib/components/ProfileTag.svelte';
+	import mattermostIcon from '$lib/assets/mattermost.svg';
+
+	type User = { id: number, username: string, mattermost: string, name: string }
+	let user: User = {
+		id: 385,
+		username: 'caturn',
+		name: 'Saturn Casaer',
+		mattermost: 'caturn'
+	};
 
 	const profileImg = 'https://zpi.zeus.gent/image/385';
-	const username = 'caturn';
+
 	const tags = [{ 'label': 'Bestuur', 'color': '255, 128, 0' }, { 'label': 'Sysadmin', 'color': '255, 0, 0' }];
 </script>
 
-<div class="flex flex-row w-full flex-wrap justify-center mb-8">
-	<img class="size-56 md:size-96 rounded-4xl m-6 mb-2" src="{profileImg}" alt="Profile">
 
+<img class="size-56 md:size-96 rounded-4xl m-6 mb-2" src="{profileImg}" alt="Profile">
+<div class="flex flex-col justify-center items-center">
 	<span
-		class="text-4xl grow min-w-64 text-center md:text-left mb-2"
-	>{ username.charAt(0).toUpperCase() + username.slice(1) }</span>
-	
+		class="text-4xl grow min-w-64 text-center md:text-left"
+	>{ user.name }</span>
+	<div class="flex flex-row justify-center gap-3 mb-4 text-xl">
+		<span>@{user.username}</span>
+		<span class="flex flex-row justify-center items-center">
+			<img class="size-3 mr-0.5" src={mattermostIcon} alt="Mattermost" />
+			{user.mattermost}
+		</span>
+	</div>
+
 	<div>
 		{#each tags as tag}
 			<ProfileTag {tag} />
 		{/each}
 	</div>
-
 </div>
+
