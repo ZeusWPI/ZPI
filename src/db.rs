a8c0f47572973a3a8ab3f33b3586663921f07a48
nebilam <49345234+Nebilam@users.noreply.github.com>
2025-08-30 14:56:03 +0200

    refactor: make DATABASE_URL public and create db in main function

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,9 @@
 use sqlx::SqlitePool;
-use sqlx::migrate::MigrateDatabase;
 use sqlx::sqlite::SqlitePoolOptions;
 use std::env;
 use std::sync::LazyLock;
 
-static DATABASE_URL: LazyLock<String> =
+pub static DATABASE_URL: LazyLock<String> =
     LazyLock::new(|| env::var("DATABASE_URL").expect("DATABASE_URL not present"));
 
 pub async fn create_conn() -> SqlitePool {
@@ -14,11 +13,4 @@ pub async fn create_conn() -> SqlitePool {
         .connect(&DATABASE_URL)
         .await
         .unwrap()
-}
-
-/// create db if it doesn't exist yet
-pub async fn create_db() {
-    sqlx::Sqlite::create_database(&DATABASE_URL)
-        .await
-        .expect("Unable to create db");
 }
