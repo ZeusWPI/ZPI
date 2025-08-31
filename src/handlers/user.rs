b225345ea1e83e729457d7c7259d62b188dad792
Hannes <hannes-dev@mailbox.org>
2025-08-31 02:42:05 +0200

    userhandler router

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,13 +1,19 @@
-use axum::Json;
+use axum::{routing::get, Json, Router};
+use sqlx::SqlitePool;
 
 use crate::{error::AppError, handlers::AuthenticatedUser};
 
 pub struct UserHandler;
 
 impl UserHandler {
-    pub async fn current_user(
+    pub fn router() -> Router<SqlitePool> {
+        Router::new().route("/me", get(Self::current_user))
+    }
+
+    async fn current_user(
         user: AuthenticatedUser,
     ) -> Result<Json<AuthenticatedUser>, AppError> {
         Ok(Json(user))
     }
 }
+ 
