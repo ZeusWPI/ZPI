860c57d4ffe73cf030d60c6f700ca62e7e18fcb1
Hannes <hannes-dev@mailbox.org>
2025-08-31 20:51:39 +0200

    split out router creation for testing

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use axum::{Router, extract::DefaultBodyLimit, response::Html, routing::get};
 use error::AppError;
 use pages::Page;
 use reqwest::StatusCode;
-use sqlx::migrate::MigrateDatabase;
+use sqlx::{SqlitePool, migrate::MigrateDatabase};
 use tokio::{
     fs,
     io::{self},
@@ -36,26 +36,46 @@ mod pages;
 async fn main() -> Result<(), Box<dyn Error>> {
     let _ = dotenvy::dotenv();
 
-    if !IMAGE_PATH.exists() {
-        fs::create_dir_all(image::IMAGE_PATH.as_path()).await?;
-    }
-
-    // create db if it doesn't exist yet
-    sqlx::Sqlite::create_database(&db::DATABASE_URL).await?;
-
+    // setup logging
     tracing_subscriber::registry()
         .with(fmt::layer())
         .with(EnvFilter::from_env("LOG_LEVEL"))
         .init();
 
-    let sess_store = MemoryStore::default();
-    let sess_mw = SessionManagerLayer::new(sess_store).with_same_site(SameSite::Lax);
-    let static_dir = ServeDir::new("./static");
+    // create image directory
+    if !IMAGE_PATH.exists() {
+        fs::create_dir_all(image::IMAGE_PATH.as_path()).await?;
+    }
 
+    // setup database
+    sqlx::Sqlite::create_database(&db::DATABASE_URL).await?;
     let db = db::create_conn().await;
     sqlx::migrate!().run(&db).await?;
 
-    let app = Router::new()
+    // setup layers
+    let sess_store = MemoryStore::default();
+    let sess_mw = SessionManagerLayer::new(sess_store).with_same_site(SameSite::Lax);
+    let app = create_router()
+        .layer(sess_mw)
+        .layer(DefaultBodyLimit::max(10_485_760))
+        .layer(CompressionLayer::new())
+        .layer(TraceLayer::new_for_http())
+        .layer(CorsLayer::permissive())
+        .with_state(db);
+
+    // start server
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+
+    Ok(())
+}
+
+pub fn create_router() -> Router<SqlitePool> {
+    let static_dir = ServeDir::new("./static");
+
+    Router::new()
         .route("/", get(index))
         .merge(AuthHandler::router())
         .nest("/image", ImageHandler::router())
@@ -67,19 +87,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 Page::error(StatusCode::NOT_FOUND, "404"),
             )
         }))
-        .layer(sess_mw)
-        .layer(DefaultBodyLimit::max(10_485_760))
-        .layer(CompressionLayer::new())
-        .layer(TraceLayer::new_for_http())
-        .layer(CorsLayer::permissive())
-        .with_state(db);
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-    axum::serve(listener, app)
-        .with_graceful_shutdown(shutdown_signal())
-        .await?;
-
-    Ok(())
 }
 
 pub async fn index(session: Session) -> Result<Html<String>, AppError> {
