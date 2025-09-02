2d047e2eaae9f950bec6ca5990d824d1e3290e9d
Hannes <hannes-dev@mailbox.org>
2025-09-02 20:15:32 +0200

    ono

--- src/lib.rs
+++ src/lib.rs
@@ -50,7 +50,7 @@ pub async fn start_app() -> Result<(), AppError> {
         .layer(DefaultBodyLimit::max(10_485_760))
         .layer(CompressionLayer::new())
         .layer(TraceLayer::new_for_http())
-        .layer(CorsLayer::very_permissive().allow_headers(Any))
+        .layer(CorsLayer::very_permissive())
         .with_state(db);
 
     // start server
