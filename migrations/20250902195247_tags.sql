40abec6d19fcade482c69fb08f00b5f5ceaab063
Hannes <hannes-dev@mailbox.org>
2025-09-02 23:25:10 +0200

    add tags to profile endpoint

---
+++ migrations/20250902195247_tags.sql
@@ -0,0 +1,17 @@
+CREATE TABLE tag
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT
+);
+
+CREATE TABLE user_tag
+(
+    user_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id)  REFERENCES tag (id) ON DELETE CASCADE,
+
+    PRIMARY KEY (user_id, tag_id)
+);
