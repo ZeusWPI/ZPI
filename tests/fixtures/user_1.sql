1cd531ee39d5ecd598ccb26c7518d0c1785e80e3
Hannes <hannes-dev@mailbox.org>
2025-09-02 17:34:08 +0200

    add test for /users/{id}

---
+++ tests/fixtures/user_1.sql
@@ -0,0 +1,4 @@
+INSERT INTO user
+    (id, username, about)
+VALUES
+    (1, "cheese", "Just a test user, doing its job... and fantasizing about a life outside the test environment.");
--- tests/user.rs
+++ tests/user.rs
@@ -1,16 +1,37 @@
 use axum::body::to_bytes;
 use reqwest::StatusCode;
 use sqlx::SqlitePool;
-use zpi::models::user::User;
+use zpi::{handlers::AuthenticatedUser, models::user::User};
 
 use crate::common::AuthenticatedRouter;
 
 mod common;
 
 #[sqlx::test]
-async fn me(db_pool: SqlitePool) {
+async fn get_users_me(db_pool: SqlitePool) {
     let router = AuthenticatedRouter::new(db_pool).await;
     let response = router.get("/users/me").await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.into_body();
+    let user_response: AuthenticatedUser =
+        serde_json::from_slice(&to_bytes(body, 1000).await.unwrap())
+            .expect("response should be valid json");
+
+    assert_eq!(
+        user_response,
+        AuthenticatedUser {
+            id: 1,
+            username: "cheese".into(),
+        }
+    );
+}
+
+#[sqlx::test(fixtures("user_1"))]
+async fn get_users_id(db_pool: SqlitePool) {
+    let router = AuthenticatedRouter::new(db_pool).await;
+    let response = router.get("/users/1").await;
 
     assert_eq!(response.status(), StatusCode::OK);
 
