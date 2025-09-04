cc2e9741b49d0b4ba75d4edd9ce320afc8623654
Hannes <hannes-dev@mailbox.org>
2025-09-05 00:38:43 +0200

    add more tests for image

--- tests/image.rs
+++ tests/image.rs
@@ -6,9 +6,33 @@ use crate::common::UnauthenticatedRouter;
 mod common;
 
 #[sqlx::test]
-async fn get_image(db_pool: SqlitePool) {
+async fn get_image_default(db_pool: SqlitePool) {
     let router = UnauthenticatedRouter::new(db_pool).await;
     let response = router.get("/image/1").await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[sqlx::test]
+async fn get_image_placeholder(db_pool: SqlitePool) {
+    let router = UnauthenticatedRouter::new(db_pool).await;
+    let response = router.get("/image/1?placeholder=true").await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[sqlx::test]
+async fn get_image_no_placeholder_404(db_pool: SqlitePool) {
+    let router = UnauthenticatedRouter::new(db_pool).await;
+    let response = router.get("/image/1?placeholder=false").await;
+
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+}
+
+#[sqlx::test]
+async fn get_image_no_placeholder(db_pool: SqlitePool) {
+    let router = UnauthenticatedRouter::new(db_pool).await;
+    let response = router.get("/image/2?placeholder=false").await;
 
     assert_eq!(response.status(), StatusCode::OK);
 }
