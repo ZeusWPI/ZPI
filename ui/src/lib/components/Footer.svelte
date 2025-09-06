e9570dad659c6d74ac859c47f0adfe240cbc41b5
The game fire <thegamefire@outlook.com>
2025-09-06 03:27:44 +0200

    Get Backend Version from Backend and Frontend Version from package.json

--- ui/src/lib/components/Footer.svelte
+++ ui/src/lib/components/Footer.svelte
@@ -1,11 +1,18 @@
-<script>
+<script lang="ts">
 	import Tooltip from '$lib/components/Tooltip.svelte';
 	import svelteIcon from '$lib/assets/svelte.svg';
 	import rustIcon from '$lib/assets/rust.svg';
+	import type { CreateQueryResult } from '@tanstack/svelte-query';
+	import { createQuery } from '@tanstack/svelte-query';
+	import { getBackendVersion } from '$lib/globalFunctions-Types';
+	import { version } from '$app/environment';
 
 
-	let backend_version = '2.0.1';
-	let frontend_version = '1.0.1';
+	let query: CreateQueryResult<String> = createQuery({
+		queryKey: ['version'],
+		queryFn: getBackendVersion
+	});
+
 </script>
 
 
@@ -15,13 +22,13 @@
 		<Tooltip text="Backend Version">
 			<div class="flex flex-row items-center">
 				<img class="size-4 mx-0.5" src={rustIcon} alt="Rust" />
-				v{backend_version}
+				v{$query.data}
 			</div>
 		</Tooltip>
 		<Tooltip text="Frontend Version">
 			<div class="flex flex-row items-center">
 				<img class="size-4 mx-0.5" src={svelteIcon} alt="Svelte" />
-				v{frontend_version}
+				v{version}
 			</div>
 		</Tooltip>
 	</div>
