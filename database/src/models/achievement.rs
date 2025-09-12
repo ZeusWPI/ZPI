319eb764d9e2b21a9cdc6e500668d184f89d25e4
Hannes <hannes-dev@mailbox.org>
2025-09-13 00:01:50 +0200

    rename FK columns to have the _id suffix

--- database/src/models/achievement.rs
+++ database/src/models/achievement.rs
@@ -1,6 +1,26 @@
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 
+pub struct Goal {
+    pub id: u32,
+    pub description: String,
+    pub achievement_id: u32,
+    pub sequence: u32,
+}
+
+pub struct Achievement {
+    pub id: u32,
+    pub name: String,
+    pub service_id: u32,
+}
+
+#[derive(Serialize)]
+pub struct AchievementPayload {
+    pub id: i32,
+    pub name: String,
+    pub goals: Vec<GoalPayload>,
+}
+
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct AchievementGoal {
     pub achievement_id: i32,
@@ -16,11 +36,4 @@ pub struct GoalPayload {
     pub id: i32,
     pub description: String,
     pub sequence: i32,
-}
-
-#[derive(Serialize)]
-pub struct AchievementPayload {
-    pub id: i32,
-    pub name: String,
-    pub goals: Vec<GoalPayload>,
 }
