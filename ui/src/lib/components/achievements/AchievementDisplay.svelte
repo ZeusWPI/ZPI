7e8b83c295b234b8d868da8c9c871fea56e9ee27
The game fire <thegamefire@outlook.com>
2025-09-17 23:30:35 +0200

    Added Achievement Edit Buttons

--- ui/src/lib/components/achievements/AchievementDisplay.svelte
+++ ui/src/lib/components/achievements/AchievementDisplay.svelte
@@ -2,6 +2,7 @@
 	import AchievementServiceGroup from '$lib/components/achievements/AchievementServiceGroup.svelte';
 	import { createQuery, type CreateQueryResult } from '@tanstack/svelte-query';
 	import { type AchievementService, getAchievementServices } from '$lib/globalFunctions-Types';
+	import AchievementEditModal from '$lib/components/achievements/AchievementEditModal.svelte';
 
 	const services = ['tap', 'zodom', 'ledstrip'];
 
@@ -12,12 +13,28 @@
 		}
 	);
 
+	const editAllowed = true;
+
+	let editModal: AchievementEditModal;
+
 </script>
 
 {#if $query.isSuccess}
-	<div class="flex flex-col items-center">
+	<div class="flex flex-col items-center w-full md:w-4/5 mx-auto px-10">
+		<div class="flex flex-row justify-end w-full mt-10">
+			<button class="bg-orange-200 hover:bg-orange-300 px-4 py-2 rounded-md font-semibold text-orange-900"
+							onclick={editModal.open}
+			>
+				Add Service
+			</button>
+		</div>
 		{#each $query.data as service}
-			<AchievementServiceGroup {service} />
+			<AchievementServiceGroup {service} editAllowed={editAllowed} editModal={editModal} />
 		{/each}
 	</div>
+
+	{#if editAllowed}
+		<AchievementEditModal bind:this={editModal} />
+	{/if}
 {/if}
+
---
+++ ui/src/lib/components/achievements/AchievementEditModal.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import { createDialog } from 'svelte-headlessui';
+	import Transition from 'svelte-transition';
+
+	const dialog = createDialog({ label: 'Edit Service' });
+
+	export function close() {
+		dialog.close();
+	}
+
+	export function open() {
+		dialog.open();
+	}
+
+	function save() {
+	}
+
+
+</script>
+<div class="relative z-10">
+	<Transition show={$dialog.expanded}>
+		<Transition
+			enter="ease-out duration-300"
+			enterFrom="opacity-0"
+			enterTo="opacity-100"
+			leave="ease-in duration-200"
+			leaveFrom="opacity-100"
+			leaveTo="opacity-0"
+		>
+			<button class="fixed inset-0 bg-black/25" aria-label="close" onclick={dialog.close}></button>
+		</Transition>
+
+		<div class="fixed inset-0 overflow-y-auto">
+			<div class="flex min-h-full items-center justify-center p-4 text-center">
+				<Transition
+					enter="ease-out duration-300"
+					enterFrom="opacity-0 scale-95"
+					enterTo="opacity-100 scale-100"
+					leave="ease-in duration-200"
+					leaveFrom="opacity-100 scale-100"
+					leaveTo="opacity-0 scale-95"
+				>
+					<div
+						class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+						use:dialog.modal
+					>
+						<h3 class="text-lg leading-6 font-medium text-gray-900">Edit Service</h3>
+						<div class="mt-2 flex flex-row justify-center">
+							// Middle Content
+						</div>
+
+						<div class="mt-4 flex flex-row justify-between">
+							<button
+								type="button"
+								class="inline-flex justify-center rounded-md border-2 border-orange-900  px-4 py-2 text-sm font-medium text-orange-900 hover:bg-orange-200 focus:outline-hidden focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2"
+								onclick={dialog.close}
+							>
+								Close
+							</button>
+							<button
+								type="button"
+								class="inline-flex justify-center rounded-md border border-transparent bg-orange-100 px-4 py-2 text-sm font-medium text-orange-900 hover:bg-orange-200 focus:outline-hidden focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2"
+								onclick={save}
+							>
+								Confirm
+							</button>
+						</div>
+					</div>
+				</Transition>
+			</div>
+		</div>
+	</Transition>
+</div>
+
