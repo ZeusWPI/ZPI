950bd06694172d7e21e228370f5c9c4f3b205276
The game fire <thegamefire@outlook.com>
2025-09-17 23:30:35 +0200

    Added Achievement Edit Buttons

--- ui/src/lib/components/achievements/AchievementServiceGroup.svelte
+++ ui/src/lib/components/achievements/AchievementServiceGroup.svelte
@@ -5,8 +5,9 @@
 	import Achievement from '$lib/components/achievements/Achievement.svelte';
 	import { createQuery, type CreateQueryResult } from '@tanstack/svelte-query';
 	import { slide } from 'svelte/transition';
+	import PencilIcon from '$lib/components/icons/PencilIcon.svelte';
 
-	let { service } = $props();
+	let { service, editAllowed, editModal } = $props();
 
 	let isOpen = $state(false);
 
@@ -19,22 +20,46 @@
 		queryFn: async () => getAchievementsFromService(service.id),
 		retry: false
 	});
+
+	function keyClickHandler(event: any, clickFn: Function) {
+		if (event.key === 'Enter' || event.key === ' ') {
+			event.preventDefault();
+			clickFn();
+		}
+	}
+
+	function openEdit() {
+		toggle();
+		editModal.open();
+	}
 </script>
 
 {#if $query.isSuccess}
-	<div class="w-full md:w-4/5">
-		<button
-			class="flex flex-row justify-between items-center w-full px-10 py-4 font-bold text-lg md:text-2xl"
-			onclick={toggle}
-			tabindex="0"
+	<div class="w-full">
+		<div role="button"
+				 class="flex flex-row justify-between items-center w-full py-4 font-bold text-lg md:text-2xl"
+				 onclick={toggle}
+				 onkeydown={(ev) => keyClickHandler(ev, toggle)}
+				 tabindex="0"
 		>
+			<span>
 			{toTitleCase(service.name)}
+				{#if editAllowed}
+					<button class="cursor-pointer rounded-md text-orange-900 bg-orange-200 hover:bg-orange-300 p-1 mx-2"
+									onclick={openEdit}
+									onkeydown={(ev) => keyClickHandler(ev, openEdit)}>
+			<span class="flex justify-center items-center size-4">
+			 <PencilIcon />
+			</span>
+	</button>
+				{/if}
+				</span>
 			<span
 				class="size-10 transition-transform duration-300 {isOpen ? 'rotate-0' : 'rotate-180'}"
 			>
 				<ArrowNoBaseIcon />
 			</span>
-		</button>
+		</div>
 
 		{#if isOpen}
 			<div transition:slide class="overflow-hidden">
