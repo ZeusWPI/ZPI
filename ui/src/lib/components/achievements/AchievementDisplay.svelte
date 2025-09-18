5407a3fd4d3c63d1cdfd55fa62d153ca56593c34
The game fire <thegamefire@outlook.com>
2025-09-18 02:23:08 +0200

    Made Achievement Edit Only Available for Admins

--- ui/src/lib/components/achievements/AchievementDisplay.svelte
+++ ui/src/lib/components/achievements/AchievementDisplay.svelte
@@ -4,7 +4,7 @@
 	import { type AchievementService, getAchievementServices } from '$lib/globalFunctions-Types';
 	import AchievementEditModal from '$lib/components/achievements/AchievementEditModal.svelte';
 
-	const services = ['tap', 'zodom', 'ledstrip'];
+	let { admin } = $props();
 
 	let query: CreateQueryResult<AchievementService[]> = createQuery({
 			queryKey: [`achievement-services`],
@@ -13,9 +13,8 @@
 		}
 	);
 
-	const editAllowed = true;
 
-	let editModal: AchievementEditModal;
+	let editModal: AchievementEditModal | undefined = $state();
 
 </script>
 
@@ -23,17 +22,17 @@
 	<div class="flex flex-col items-center w-full md:w-4/5 mx-auto px-10">
 		<div class="flex flex-row justify-end w-full mt-10">
 			<button class="bg-orange-200 hover:bg-orange-300 px-4 py-2 rounded-md font-semibold text-orange-900"
-							onclick={editModal.open}
+							onclick={editModal?.open}
 			>
 				Add Service
 			</button>
 		</div>
 		{#each $query.data as service}
-			<AchievementServiceGroup {service} editAllowed={editAllowed} editModal={editModal} />
+			<AchievementServiceGroup {service} editAllowed={admin} editModal={editModal} />
 		{/each}
 	</div>
 
-	{#if editAllowed}
+	{#if admin}
 		<AchievementEditModal bind:this={editModal} />
 	{/if}
 {/if}
